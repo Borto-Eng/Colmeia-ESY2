735ebc03dc721fa14ec61e63fd02bc514db431f4
Bortollotti Vinicius (PS/ESY2-LA) <bov1ct@bosch.com>
2026-09-04 15:59:33 -0300

    alterado logo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,24 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home"
-import SilverElectrode from "../pages/SilverElectrode";
-import Calendar from "../pages/Calendar";
-import Forms from "../pages/Forms";
+
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Home/>
     },
-    {
-        path: '/silverelectrode',
-        element: <SilverElectrode/>
-    },
-    {
-        path: '/calendar',
-        element: <Calendar/>
-    },
-    {
-        path: '/forms',
-        element: <Forms/>
-    }
-]) 
+    
+])
