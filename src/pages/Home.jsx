ad3939cacce247d7739e7b38a5e92dfaeba81444
Bortollotti Vinicius (PS/ESY2-LA) <bov1ct@bosch.com>
2026-09-09 12:54:27 -0300

    Calendario

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,12 @@ export default function Home() {
   const meusLinks = [
     {title: 'Guideline Calibração', link: 'https://inside-docupedia.bosch.com/confluence/calibrationtrainingwiki/all/en/calibration-training-wiki-home-730767881.html', relevance: 0, color: { from: '#c4d9ffff', to: '#3987eeff' }},
     {title: 'VDS', link: 'https://ens-la.bosch-vds.com/',relevance: 0, color: { from: 'rgb(80, 80, 81)', to: 'rgb(197, 198, 199)' }},
-    {title: 'BD Links', link: 'https://bdweblaserviceslocations.bosch.com/MyBDLinksLA/home', relevance: 0, color: { from: '#c4d9ffff', to: '#3987eeff' }}, 
+    {title: 'My BD Links', link: 'https://bdweblaserviceslocations.bosch.com/MyBDLinksLA/home', relevance: 0, color: { from: '#c4d9ffff', to: '#3987eeff' }}, 
     {title: 'Cardápio', link: 'https://ctweb03.br.bosch.com/presenter/#!/presentation',relevance: 2, color: { from: 'rgb(48, 48, 210)', to: 'rgb(197, 198, 199)' }},
     {title: 'Tool Catalog', link: 'https://rb-tools.de.bosch.com/home',relevance: 0, color: { from: 'rgb(80, 80, 81)', to: 'rgb(197, 198, 199)' }}, 
     {title: 'Gestão de NFs', link: 'https://fde-nota-fiscal-34229078-edcnb0fretftb7bx.a02.azurefd.net/',relevance: 0, color: { from: 'rgb(80, 80, 81)', to: 'rgb(197, 198, 199)' }}, 
     {title: 'xORDER', link: 'https://rb-xorder.bosch.com/',relevance: 1, color: { from: 'rgb(21, 175, 157)', to: 'rgb(104, 145, 150)' }}, 
+    {title: 'Calendário', link: '/calendar',relevance: 2, color: { from: 'rgb(4, 67, 175)', to: 'rgb(179, 179, 205)' }},
   ];
   
   return <Colmeia links={meusLinks} />;
